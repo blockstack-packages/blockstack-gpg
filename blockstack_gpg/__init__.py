c1aef3e2ea90019ae06513a45abfac2aabde68d2
Jude Nelson <judecn@gmail.com>
2016-04-25 17:32:28 -0400

    expose encrypt/decrypt

--- blockstack_gpg/__init__.py
+++ blockstack_gpg/__init__.py
@@ -26,3 +26,4 @@ from gpg import gpg_profile_create_key, gpg_profile_put_key, gpg_profile_delete_
 from gpg import gpg_list_app_keys, gpg_list_profile_keys
 from gpg import gpg_fetch_key
 from gpg import gpg_profile_get_key, gpg_app_get_key
+from gpg import gpg_encrypt, gpg_decrypt
